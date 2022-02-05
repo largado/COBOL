3bda39b75f10ffd3cc339eea403114e3f4604c73
largado <49825687+largado@users.noreply.github.com>
2022-02-05 20:01:52 -0300

    Update PROGCOB01.cob

--- PROGCOB01.cob
+++ PROGCOB01.cob
@@ -1,1 +1,17 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello Alex"
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
 
